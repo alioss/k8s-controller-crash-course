e6316fd65382cdcae53cfe020ececc092f6f759b
Andrii Bushchuk <andy.bushchuk@gmail.com>
2025-06-20 22:54:28 -0400

    Add step2: Zerolog for Log Levels

---
+++ cmd/root.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "k8s-controller-tutorial",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+		log.Info().Msg("This is an info log")
+		log.Debug().Msg("This is a debug log")
+		log.Trace().Msg("This is a trace log")
+		log.Warn().Msg("This is a warn log")
+		log.Error().Msg("This is an error log")
+		fmt.Println("Welcome to k8s-controller-tutorial CLI!")
+	},
+}
+
+func Execute() {
+	rootCmd.Execute()
+}
